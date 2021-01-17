8459927f61bd38ec6070d33049eb986e4d9d30ae
liqin323 <liqin323@gmail.com>
2021-01-17 12:09:48 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.showString("Hello!")
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
